c744011562ac2807eb5e8203594a281494432e9a
Sharmaine Requerme <srqrme@gmail.com>
2018-10-18 17:53:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,14 +9,13 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/best-netflix-movies-to-watch-right-now/"))
 
-    doc.css(".row.countdown-item").each do |row|
-      course = Course.new
-      course.title = row.css(".article_movie_title h2 a").text
-      course.rank = row.css(".countdown-index").text
-      course.consensus = row.css(".info.critics-consensus").text.split("Critics Consensus:").join
-      course.movie_url = row.css(".article_movie_title div h2 a").attribute("href").value
-      binding.pry
-    end
+    #doc.css(".row.countdown-item").each do |row|
+      #course = Course.new
+      #course.title = row.css(".article_movie_title h2 a").text
+      #course.rank = row.css(".countdown-index").text
+      #course.consensus = row.css(".info.critics-consensus").text.split("Critics Consensus:").join
+      #course.movie_url = row.css(".article_movie_title div h2 a").attribute("href").value
+    #end
   end
 
   def get_courses
